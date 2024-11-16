9dd21d5f541d428798216a6ee22e916f2eb0b989
sohseyedi <sohseyedi@gmail.com>
2024-11-16 17:27:06 +0330

    category data

---
+++ constant/cateogry.js
@@ -0,0 +1,8 @@
+export const CategoryLists = [
+    { id: '1', title: 'All' },
+    { id: '2', title: 'Puma' },
+    { id: '3', title: 'Addidas' },
+    { id: '4', title: 'Nike' },
+    { id: '5', title: 'Skechers' },
+    { id: '6', title: 'Vans' }
+];
